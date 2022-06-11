60616924514d61d10f143b5fef9660181ad5822a
Kenya Nonaka <kenya.nonaka@uzabase.com>
2022-06-11 23:10:44 +0900

    rafactor: Francの__eq__も一般化

--- tdd_by_python/money.py
+++ tdd_by_python/money.py
@@ -8,12 +8,6 @@ class Doller(Money):
     def times(self, multiplier):
         return Doller(self._amount * multiplier)
     
-class Franc:
-    def __init__(self, amount):
-        self.__amount = amount
-
+class Franc(Money):
     def times(self, multiplier):
-        return Franc(self.__amount * multiplier)
-    
-    def __eq__(self, obj):
-        return self.__amount == obj.__amount
+        return Franc(self._amount * multiplier)
