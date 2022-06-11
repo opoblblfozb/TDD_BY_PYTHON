0cf132ecc36f7194312eee20b49250dbf7603a7c
Kenya Nonaka <kenya.nonaka@uzabase.com>
2022-06-11 23:40:38 +0900

    green: 一旦、クラス名を比較する形でテストを切り抜けた

--- tdd_by_python/money.py
+++ tdd_by_python/money.py
@@ -1,13 +1,19 @@
 class Money:
     def __init__(self, amount):
         self._amount = amount
+
     def __eq__(self, obj):
-        return self._amount == obj._amount
+        return (
+            self._amount == obj._amount
+            and self.__class__.__name__ == obj.__class__.__name__
+        )
+
 
 class Doller(Money):
     def times(self, multiplier):
         return Doller(self._amount * multiplier)
-    
+
+
 class Franc(Money):
     def times(self, multiplier):
         return Franc(self._amount * multiplier)
