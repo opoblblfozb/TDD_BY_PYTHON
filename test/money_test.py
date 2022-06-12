9209d770e35be9dc6f1df14d7f1bdd62611a1e61
Kenya Nonaka <kenya.nonaka@uzabase.com>
2022-06-12 15:13:39 +0900

    red: DollerとFrancの重複を削除するためにインターフェイスを変更した

--- test/money_test.py
+++ test/money_test.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
-import pytest
 
-from tdd_by_python.money import Doller, Franc
+from tdd_by_python.money import Doller, Franc, Money
 
 """
 TODO
@@ -17,20 +16,21 @@ timesの一般化
 DollerとFrancの比較
 """
 
+
 class MoneyTest(TestCase):
     def test_doller_multiplication(self):
-        five = Doller(5)
-        self.assertEqual(Doller(10), five.times(2))
-        self.assertEqual(Doller(15), five.times(3))
+        five = Money.doller(5)
+        self.assertEqual(Money.doller(10), five.times(2))
+        self.assertEqual(Money.doller(15), five.times(3))
 
     def test_franc_multiplication(self):
         five = Franc(5)
         self.assertEqual(Franc(10), five.times(2))
         self.assertEqual(Franc(15), five.times(3))
-    
+
     def test_equal(self):
-        self.assertTrue(Doller(5).__eq__(Doller(5)))
-        self.assertFalse(Doller(5).__eq__(Doller(6)))
+        self.assertTrue(Money.doller(5).__eq__(Money.doller(5)))
+        self.assertFalse(Money.doller(5).__eq__(Money.doller(6)))
         self.assertTrue(Franc(5).__eq__(Franc(5)))
         self.assertFalse(Franc(5).__eq__(Franc(6)))
-        self.assertFalse(Doller(15).__eq__(Franc(15)))
+        self.assertFalse(Money.doller(15).__eq__(Franc(15)))
