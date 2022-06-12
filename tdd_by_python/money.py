f56168d5cb12698525d736cf0257b4a8c6978fec
Kenya Nonaka <kenya.nonaka@uzabase.com>
2022-06-12 17:14:18 +0900

    times内で、self._currencyを参照するように変更

--- tdd_by_python/money.py
+++ tdd_by_python/money.py
@@ -23,9 +23,9 @@ class Money:
 
 class Doller(Money):
     def times(self, multiplier):
-        return Money(self._amount * multiplier, "USD")
+        return Money(self._amount * multiplier, self._currency)
 
 
 class Franc(Money):
     def times(self, multiplier):
-        return Money(self._amount * multiplier, "CHF")
+        return Money(self._amount * multiplier, self._currency)
