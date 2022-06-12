5e12bcb844604f631683be4d659c1ca9b8bf37b2
Kenya Nonaka <kenya.nonaka@uzabase.com>
2022-06-12 16:00:49 +0900

    refactor: サブクラスに実装されているtimesの中で、MoneyのFactoryメソッドを使うようにした。コンストラクタを使いたくない!

--- tdd_by_python/money.py
+++ tdd_by_python/money.py
@@ -30,9 +30,9 @@ class Money(metaclass=ABCMeta):
 
 class Doller(Money):
     def times(self, multiplier):
-        return Doller(self._amount * multiplier, "USD")
+        return Money.doller(self._amount * multiplier)
 
 
 class Franc(Money):
     def times(self, multiplier):
-        return Franc(self._amount * multiplier, "CHF")
+        return Money.franc(self._amount * multiplier)
