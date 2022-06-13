392d4217e5fe3e7874d16e3c879404a4779b061b
Kenya Nonaka <kenya.nonaka@uzabase.com>
2022-06-13 23:54:28 +0900

    refactor: reduceの実装をSumの方に押し込んだ。（インスタンス変数にアクセスしているためpytest

--- tdd_by_python/bank.py
+++ tdd_by_python/bank.py
@@ -7,5 +7,4 @@ from .money import Money, Sum
 class Bank:
     def reduce(self, source: Expression, to: str) -> Money:
         sum: Sum = source
-        amount: int = sum.addend._amount + sum.augend._amount
-        return Money(amount, to)
+        return sum.reduce(to)
