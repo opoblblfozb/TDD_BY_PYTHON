fba315161f6831e2eedd3efcaf38cfe0ae76a5e6
Kenya Nonaka <kenya.nonaka@uzabase.com>
2022-06-13 21:23:37 +0900

    不要になったテストを削除した。

--- test/money_test.py
+++ test/money_test.py
@@ -23,21 +23,11 @@ class MoneyTest(TestCase):
         self.assertEqual(Money.doller(10), five.times(2))
         self.assertEqual(Money.doller(15), five.times(3))
 
-    def test_franc_multiplication(self):
-        five = Money.franc(5)
-        self.assertEqual(Money.franc(10), five.times(2))
-        self.assertEqual(Money.franc(15), five.times(3))
-
     def test_equal(self):
         self.assertTrue(Money.doller(5).__eq__(Money.doller(5)))
         self.assertFalse(Money.doller(5).__eq__(Money.doller(6)))
-        self.assertTrue(Money.franc(5).__eq__(Money.franc(5)))
-        self.assertFalse(Money.franc(5).__eq__(Money.franc(6)))
         self.assertFalse(Money.doller(15).__eq__(Money.franc(15)))
 
     def test_currency(self):
         self.assertEqual(Money.doller(5).currency(), "USD")
         self.assertEqual(Money.franc(5).currency(), "CHF")
-
-    def test_diffrentclass_equality(self):
-        self.assertEqual(Money(5, "CHF"), Franc(5, "CHF"))
